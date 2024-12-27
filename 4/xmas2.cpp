fb901f7b42e4e73b530b84b19f576361a9e9f670
Jakenv <bartoszmarciniak7258@protonmail.com>
2024-12-27 18:19:45 +0100

    AoC Day4 - 2 stars

---
+++ 4/xmas2.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Provide path to file" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if (!file.is_open())
+    {
+        std::cerr << "Can' open file" << std::endl;
+        return 1;
+    }
+
+    int hight = 140;
+    std::vector<std::string> vec(hight);
+    for (std::string &row : vec)
+        file >> row;
+
+    int width = (int)vec[0].length();
+    const std::vector<std::pair<int, int>> directions = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+
+    int answer = 0;
+    for (int row = 1; row < hight - 1; row++)
+    {
+        for (int col = 1; col < width - 1; col++)
+        {
+            if (vec[row][col] == 'A')
+            {
+                std::string s;
+                for (std::pair<int, int> direction : directions)
+                    s += vec[row + direction.first][col + direction.second];
+                if (s == "MMSS" || s == "MSSM" || s == "SMMS" || s == "SSMM")
+                    answer++;
+            }
+        }
+    }
+    std::cout << answer << "\n";
+}
